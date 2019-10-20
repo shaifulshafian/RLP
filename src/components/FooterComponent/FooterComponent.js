66e1db654cbbfe7fb7d9493beae229ce65aaadb9
Shaiful Shafian <shaifulshafian@gmail.com>
2019-10-21 05:08:17 +0800

    first commit

---
+++ src/components/FooterComponent/FooterComponent.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./style.css";
+
+export const FooterComponent = props => {
+  return (
+    <footer>
+      <div className="container">
+        <p>&copy; 2019 Kenari Sdn Bhd. All Rights Reserved.</p>
+        <ul className="list-inline">
+          <li className="list-inline-item">
+            <a href="#">Privacy</a>
+          </li>
+          <li className="list-inline-item">
+            <a href="#">Terms</a>
+          </li>
+          <li className="list-inline-item">
+            <a href="#">FAQ</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
